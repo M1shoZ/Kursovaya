021e7b131d2751a862075410f96cc8686637d76c
Michael Zakaryan <mikzakaryan@mail.ru>
2023-07-01 14:35:54 +0300

    -Создал пустое окно приложение -Прописал логику для кнопки войти для перехода на окно приложения

--- src/main/java/com/example/kursovaya/Controller.java
+++ src/main/java/com/example/kursovaya/Controller.java
@@ -1,5 +1,81 @@
 package com.example.kursovaya;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class Controller {
 
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private Button loginRegButton;
+
+    @FXML
+    private PasswordField passField;
+
+    @FXML
+    void initialize() {
+        loginButton.setOnAction(actionEvent -> {
+            System.out.println("Вы нажали кноку Войти");
+//            openWindow("/resources/com.example.kursovaya/app.fxml");
+
+            loginButton.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("app.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+
+//            loginButton.getScene().getWindow().hide();
+        });
+    }
+    public void openWindow(String window){
+        loginButton.getScene().getWindow().hide();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(window));
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Parent root = loader.getRoot();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.showAndWait();
+    }
 }
+
+
---
+++ src/main/java/com/example/kursovaya/appController.java
@@ -0,0 +1,20 @@
+package com.example.kursovaya;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class appController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
